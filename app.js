103417c9da81229212dc725510857941e69ff704
Muath <Muath.khalifa@yahoo.com>
2022-09-14 07:20:15 +0100

    reset head here if things break

--- app.js
+++ app.js
@@ -11,59 +11,57 @@ var y = 0
 const word = "hello";
 const arr = word.split("");
 var input = [];
+var ev = [];
+const sim = () => input.map((char, i) => {
+    const index = arr.indexOf(char)
+    console.log(`Char: ${char} i:${i} index:${index}`)
+    return index !== -1 && index !== i
+  }) //=> [true, true]
 
-const found = input.some(r=> arr.indexOf(r) >= 0)
+// console.log(sim())
 
 var z = 0;
 for (const letter of letters){
+    console.log("Loops")
     letter.addEventListener("click", (e)=>{
-        var event =  e.target.innerText
-        console.log(event)
-        if(y == 5 || y == 10 || y==15 || y==20 || y== 25){
-            console.log("press enter")
-            console.log(y)
-        }
 
 
-        if(y != 5 && y != 10 && y!=15 && y!=20 && 25){
-        // if(2+2==4){
+        if(y == 0 || y % 5 != 0){
             i++
             y++
             boxes[i-1].value = letter.innerText;
             y = i ;
-            // console.log(`heretoo i:${i}  y:${y}`)
             input.push(letter.innerText)
+            console.log(input)
             
         }
         enter.addEventListener("click", ()=>{
-            console.log("before", e.target.innerText)
+            console.log("enter")
+            ev.push(e.target.innerText)
+            ev = ev.slice(-1);
             y++;
             
-            if(y!=0 && y%5 == 0){
-                y+=1.2
+            // if(y!=0 && y%5 == 0){
+            //     y+=1.2
 
-                // if(!arr.includes(letter.innerText.toLowerCase())){
-                //     letter.style.background = "red"
-                //     boxes[i-1].style.background = "red"
-                // }
 
                 
-                for(var i = 0; i < arr.length; i++){
-                    if(arr[i].toLowerCase() == input[i].toLowerCase()){
-                        boxes[z].style.background = "green"
-                        z++
+            //     for(var i = 0; i < arr.length; i++){
+                    
+            //         if(arr[i].toLowerCase() == input[i].toLowerCase()){
+            //             boxes[z].style.background = "green"
+            //             z++
                         
-                    }else{
-                        boxes[z].style.background = "red"
-                        e.target.style.background = "red"
-                        z++
-                    }
-                }
+            //         }else{
+            //             boxes[z].style.background = "red"
+            //             z++
+            //         }
+            //     }
+            //     // ev = [];
+            //     input = [];
 
-                input = [];
-
-            }else{
-            }
+            // }else{
+            // }
             
     })
     
