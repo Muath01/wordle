f605e94ae5e3c2cb9d0bc53677e9f9b3c5f969e6
Muath <Muath.khalifa@yahoo.com>
2022-09-14 03:56:40 +0100

    added event target to for loop

--- app.js
+++ app.js
@@ -12,17 +12,19 @@ const word = "hello";
 const arr = word.split("");
 var input = [];
 
+const found = input.some(r=> arr.indexOf(r) >= 0)
+
 var z = 0;
 for (const letter of letters){
-    letter.addEventListener("click", ()=>{
-        
+    letter.addEventListener("click", (e)=>{
+        var event =  e.target.innerText
+        console.log(event)
         if(y == 5 || y == 10 || y==15 || y==20 || y== 25){
             console.log("press enter")
             console.log(y)
         }
 
 
-        console.log("before the if")
         if(y != 5 && y != 10 && y!=15 && y!=20 && 25){
         // if(2+2==4){
             i++
@@ -31,15 +33,14 @@ for (const letter of letters){
             y = i ;
             // console.log(`heretoo i:${i}  y:${y}`)
             input.push(letter.innerText)
+            
         }
         enter.addEventListener("click", ()=>{
+            console.log("before", e.target.innerText)
             y++;
             
             if(y!=0 && y%5 == 0){
                 y+=1.2
-                console.log(y)
-                console.log("isnide the f")
-
 
                 // if(!arr.includes(letter.innerText.toLowerCase())){
                 //     letter.style.background = "red"
@@ -49,12 +50,13 @@ for (const letter of letters){
                 
                 for(var i = 0; i < arr.length; i++){
                     if(arr[i].toLowerCase() == input[i].toLowerCase()){
-                        console.log("EQUAL")
                         boxes[z].style.background = "green"
                         z++
-                        console.log("z:",z)
+                        
                     }else{
-                        console.log("No EQUAL")
+                        boxes[z].style.background = "red"
+                        e.target.style.background = "red"
+                        z++
                     }
                 }
 
