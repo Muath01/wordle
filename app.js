e70d326aef41e8167d284bb839ab359c1dafe3b0
Muath <Muath.khalifa@yahoo.com>
2022-09-14 11:38:52 +0100

    added a delete button

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const letters = document.getElementsByClassName("letter");
 const boxes = document.getElementsByClassName("box");
-const enter = document.getElementsByClassName("enter")[0]
+const enter = document.getElementById("enter");
+const deleteLetter = document.getElementById("delete");
 
 
 
@@ -8,9 +9,7 @@ const enter = document.getElementsByClassName("enter")[0]
 var i = 0
 var y = 0
 
-
-
-const word = "hello";
+const word = "crape";
 const arr = word.split("");
 var input = [];
 var ev = [];
@@ -19,9 +18,7 @@ let letterDiv = [];
 
 var z = 0;
 for (const letter of letters){
-    console.log("Loops")
     letter.addEventListener("click", (e)=>{
-
 
         if(y == 0 || y % 5 != 0){
             i++
@@ -33,41 +30,38 @@ for (const letter of letters){
             
         }})}
 
-
-
 enter.addEventListener("click", ()=>{
 
-
-
-
-    console.log(letterDiv[letterDiv.length-1].innerText)
-    
     ev = ev.slice(-1);
     
     if(y!=0 && y%5 == 0){
+    
+
         y++
         const sim = () => input.map((char, i) => {
             const index = arr.indexOf(char)
+
             // console.log(`Char: ${char} i:${i} index:${index}`)
             return index !== -1 &&  index !== i
           }) //=> [true, true]
 
         for(var i = 0; i < arr.length; i++){
             const simx = sim();
-            console.log(simx[0])
             if(arr[i].toLowerCase() == input[i].toLowerCase()){
                 boxes[z].style.background = "green"
                 letterDiv[i].style.background = "green"
                 z++
                 
             }else if(simx[i] === true){
-                console.log("here")
-                letterDiv[i].style.background = "yellow"
+                boxes[z].style.background = "yellow"
+                boxes[i].style.background = "yellow"
+                z++
+
             }
             
-            else{
+            else if (simx[i] == false){
                 boxes[z].style.background = "red"
-                // letterDiv[i].style.background = "red"
+                letterDiv[i].style.background = "red"
                 z++
             }
         }
@@ -77,5 +71,12 @@ enter.addEventListener("click", ()=>{
     }else{
     }
     
+})
+
+deleteLetter.addEventListener("click", ()=>{
+    input.pop();
+    letterDiv.pop()
+    boxes[i-1].value = "";
+    i--;
 })
     
